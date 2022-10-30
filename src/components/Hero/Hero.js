cffa43d94caeea552afea56aa46cde11658285b7
jb <jasontbrown99@gmail.com>
2022-10-29 18:12:09 -0700

    adds hosted, optimized fonts, uses avif, accessibility adjustments

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,8 +4,26 @@ import styled from 'styled-components/macro';
 const Hero = () => {
   return (
     <Wrapper>
-      <HeroImage src="/images/hero-img.jpg" />
-      <Swoop src="/swoop.svg" />
+      <picture>
+        <source 
+          type='image/avif'
+          srcSet={`
+            /images/hero-img.avif 1x,
+            /images/hero-img@2x.avif 2x,
+            /images/hero-img@3x.avif 3x,
+          `}
+        />
+        <source 
+          type='image/jpeg'
+          srcSet={`
+            /images/hero-img.jpg 1x,
+            /images/hero-img@2x.jpg 2x,
+            /images/hero-img@3x.jpg 3x,
+          `}
+        />
+      </picture>
+      <HeroImage src="/images/hero-img.jpg" alt='Large image of cat showing an example of sweet images you can get from this site.'/>
+      <Swoop src="/swoop.svg" alt=''/>
     </Wrapper>
   );
 };
